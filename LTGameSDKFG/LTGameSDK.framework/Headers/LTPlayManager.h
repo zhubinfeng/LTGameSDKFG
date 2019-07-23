293f70069d0b5b354e0b80b1552f51e3100c7eaa
zhubinfeng <zhubin_feng@163.com>
2019-07-23 15:06:20 +0800

    update framework

--- LTGameSDKFG/LTGameSDK.framework/Headers/LTPlayManager.h
+++ LTGameSDKFG/LTGameSDK.framework/Headers/LTPlayManager.h
@@ -24,42 +24,15 @@ typedef void(^playFailureBlocks)(int error, NSString *errorInfoMessage);
 // pay now status
 typedef void(^playNowStatusBlocks)(int code, NSString *nowInfoMessage);
 
-// !!!: - ================================= Buy delegate
 @class TLPlayManager;
-@protocol delegatePlay <NSObject>
-//=========================================
-// If using a delegate, Must be realized,
-// If using Blocks,Don't realized
-// 如果使用了代理，必须实现下面的回调，
-// 如果使用了Block可以不用实现
-//=========================================
-@required
-// Pay Success delegate
--(void)playSuccessDelegate:(int)code secuessInfoData:(NSDictionary *)secuessInfoData secuessInfoMessage:(NSString *)secuessInfoMessage;
-// Pay failure delegate
--(void)playFailureDelegate:(int)error errorInfoMessage:(NSString *)errorInfoMessage;
-//
-@optional
 
-// Pay now status
--(void)playNowStatusDelegate:(int)code nowInfoMessage:(NSString *)nowInfoMessage;
-
-@end
-
-
-// !!!: - =================================
 @interface LTPlayManager : NSObject
-///
--(instancetype)initPlayWithViewModel:(GoodsModel *)goodsModel SuccessBlocks:(playSuccessBlocks)successBlocks failureBlocks:(playFailureBlocks)failureBlocks;
 
++ (instancetype)sharedInstance;
 
+-(void)playWithViewModel:(GoodsModel *)goodsModel SuccessBlocks:(playSuccessBlocks)successBlocks failureBlocks:(playFailureBlocks)failureBlocks  nowStatusBlocks:(playNowStatusBlocks)nowStatusBlocks;
 /// Product information 订单信息
 @property (strong, nonatomic, nonnull) GoodsModel *goodsModel;
-
-/// 当前支付状态 Blocks
-@property (copy, nonatomic) playNowStatusBlocks nowStatusBlocks;
-
-@property (weak, nonatomic) id <delegatePlay> delegatePlay;
 
 @end
 
